ceef9b0532af616a758d3d67b979fcee7862fc6d
Neesar Banna <Bannasqe@gmail.com>
2023-10-25 17:45:58 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+const h3 =document.createElement("h3")
+h3.textContent = "Test Js script 2"
+
+document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h3);
